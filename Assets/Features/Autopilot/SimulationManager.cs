fd635fd81c9dff0eb5420017665a590e346ff677
knamsayin <lukejordensnz@gmail.com>
2023-08-13 21:00:26 +1200

    Updated simulation manager.
    Added new functionality to send lap data from unity to python

--- Assets/Features/Autopilot/SimulationManager.cs
+++ Assets/Features/Autopilot/SimulationManager.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Perrinn424.AutopilotSystem;
+using System.Net.Sockets;
+using System.Text;
+using System;
 
 public class SimulationManager : MonoBehaviour
 {
@@ -9,17 +12,23 @@ public class SimulationManager : MonoBehaviour
     private int lapCount = 0;
     private string[] assetPaths = {"GeneticAssets/asset1", "GeneticAssets/asset2"};
     private int currentAssetIndex = 0;
+    private LapTimesWrapper lapData;
 
     void Start()
     {
         autopilot.enabled = true;
         LoadLap(assetPaths[currentAssetIndex]);
+        // Initialize the LapTimesWrapper with test data
+        lapData = new LapTimesWrapper();
     }
 
     void Update()
 {
     if (autopilot.HasCompletedLap)
     {
+        Debug.Log("Lap times before sendLapTimes method call: " + string.Join(", ", lapData.lapTimes));
+        Debug.Log("Update method called");
+        sendLapTimes();
         float finalLapTime = autopilot.GetFinalLapTime();
         Debug.Log("Final lap time: " + finalLapTime);
 
@@ -28,8 +37,9 @@ public class SimulationManager : MonoBehaviour
 
         if (lapCount > 1)
         {
-            // Load the next asset file
-            currentAssetIndex = (currentAssetIndex + 1) % assetPaths.Length;
+                
+                // Load the next asset file
+                currentAssetIndex = (currentAssetIndex + 1) % assetPaths.Length;
             RecordedLap newLap = Resources.Load<RecordedLap>(assetPaths[currentAssetIndex]);
             autopilot.recordedLap = newLap;
 
@@ -53,6 +63,11 @@ public class SimulationManager : MonoBehaviour
 }
 
 
+    [Serializable]
+    public class LapTimesWrapper
+    {
+       [SerializeField] public float[] lapTimes = { 3.32342f, 3.3f, 4.4f };
+    }
     private void LoadLap(string assetPath)
     {
         RecordedLap newLap = Resources.Load<RecordedLap>(assetPath);
@@ -67,4 +82,39 @@ public class SimulationManager : MonoBehaviour
             Debug.LogError("Failed to load asset: " + assetPath);
         }
     }
+
+    public void sendLapTimes()
+    {
+        try
+        {
+            using (TcpClient client = new TcpClient())
+            {
+                client.Connect("127.0.0.1", 65432);
+                using (NetworkStream stream = client.GetStream())
+                {
+                    string jsonData = JsonUtility.ToJson(lapData);
+                    Debug.Log("Serialized lap data: " + jsonData);
+                    jsonData += "<EOF>";
+                    byte[] data = Encoding.UTF8.GetBytes(jsonData);
+
+                    stream.Write(data, 0, data.Length);
+
+                    // Read server response
+                    byte[] responseData = new byte[256];
+                    int bytes = stream.Read(responseData, 0, responseData.Length);
+                    string response = Encoding.UTF8.GetString(responseData, 0, bytes);
+                    Debug.Log("Server response: " + response);
+                }
+            }
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError("Error while sending lap times: " + e.Message);
+        }
+    }
+
+
+
+
 }
+
